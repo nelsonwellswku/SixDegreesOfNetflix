1f385ae0b8ac3207490c4311ae24383fd1af6e61
Nelson Wells <nelsonwellswku@gmail.com>
2017-07-29 18:14:28 -0500

    Implement GetSingleAsync in netflix roulette client

--- Application/NetflixRoulette/NetflixRouletteClient.cs
+++ Application/NetflixRoulette/NetflixRouletteClient.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +9,42 @@ namespace Octogami.SixDegreesOfNetflix.Application.NetflixRoulette
 {
     public class NetflixRouletteClient
     {
-        public HttpClient HttpClient { private get; set; }
-
         public NetflixRouletteClient()
         {
             HttpClient = new HttpClient {BaseAddress = new Uri("https://netflixroulette.net")};
         }
 
+        public HttpClient HttpClient { private get; set; }
+
         /// <summary>
-        /// Get a single response object. Use this when searching by title or title and year.
+        ///     Get a single response object. Use this when searching by title or title and year.
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        public Task<(NetflixRouletteResponse, NetflixRouletteError)> GetSingleAsync(NetflixRouletteRequest request)
+        public async Task<(NetflixRouletteResponse, NetflixRouletteError)> GetSingleAsync(
+            NetflixRouletteRequest request)
         {
-            throw new NotImplementedException();
+            var url = BuildRequestUrl(request);
+            var httpResponse = await HttpClient.GetAsync(url);
+            var jsonContent = await httpResponse.Content.ReadAsStringAsync();
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var successResponse = JsonConvert.DeserializeObject<NetflixRouletteResponse>(jsonContent);
+                return (successResponse, null);
+            }
+
+            var errorResponse = JsonConvert.DeserializeObject<NetflixRouletteError>(jsonContent);
+            return (null, errorResponse);
         }
 
         /// <summary>
-        /// Get many response objects. Use this when searching by actor or director.
+        ///     Get many response objects. Use this when searching by actor or director.
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        public async Task<(List<NetflixRouletteResponse>, NetflixRouletteError)> GetManyAsync(NetflixRouletteRequest request)
+        public async Task<(List<NetflixRouletteResponse>, NetflixRouletteError)> GetManyAsync(
+            NetflixRouletteRequest request)
         {
             var url = BuildRequestUrl(request);
             var httpResponse = await HttpClient.GetAsync(url);
@@ -54,24 +66,16 @@ namespace Octogami.SixDegreesOfNetflix.Application.NetflixRoulette
             var stringBuilder = new StringBuilder(baseUrl);
 
             if (request.Title != null)
-            {
                 stringBuilder.Append($"title={request.Title}");
-            }
 
             if (request.Year != 0)
-            {
                 stringBuilder.Append($"&year={request.Year}");
-            }
 
             if (request.Actor != null)
-            {
                 stringBuilder.Append($"actor={request.Actor}");
-            }
 
             if (request.Director != null)
-            {
                 stringBuilder.Append($"director={request.Director}");
-            }
 
             return stringBuilder.ToString();
         }
