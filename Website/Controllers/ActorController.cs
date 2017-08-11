4f92d9a2472fc3993d5feb242999637a78195ff4
Nelson Wells <nelsonwellswku@gmail.com>
2017-08-10 22:37:52 -0500

    Set up two pages, one with form elements and one to render result

---
+++ Website/Controllers/ActorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Octogami.SixDegreesOfNetflix.Website.Controllers
+{
+    public class ActorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Degrees(string actorOne, string actorTwo)
+        {
+            return View();
+        }
+    }
+}
