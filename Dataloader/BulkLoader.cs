e4cf9d722084a0f27b5546e8c3569e5f4335a458
Nelson Wells <nelsonwellswku@gmail.com>
2020-03-31 20:54:06 -0500

    Add a data loader project to bulk insert records into CosmosDB

---
+++ Dataloader/BulkLoader.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.CosmosDB.BulkExecutor.Graph;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace Octogami.SixDegreesOfNetflix.Dataloader
+{
+    public interface IBulkLoader
+    {
+        Task BulkInsertAsync(IEnumerable<object> graphElements, CancellationToken token = default(CancellationToken));
+    }
+
+    public class BulkLoader : IBulkLoader
+    {
+        private CosmosConfiguration _cosmosConfiguration;
+
+        public BulkLoader(CosmosConfiguration cosmosConfiguration)
+        {
+            _cosmosConfiguration = cosmosConfiguration;
+        }
+
+        public async Task BulkInsertAsync(IEnumerable<object> graphElements, CancellationToken token = default(CancellationToken))
+        {
+            ConnectionPolicy connectionPolicy = new ConnectionPolicy
+            {
+                ConnectionMode = ConnectionMode.Direct,
+                ConnectionProtocol = Protocol.Tcp
+            };
+
+            var documentClient = new DocumentClient(
+                new Uri(_cosmosConfiguration.Host),
+                _cosmosConfiguration.Password,
+                connectionPolicy
+            );
+
+            await documentClient.CreateDatabaseIfNotExistsAsync(new Database() { Id = _cosmosConfiguration.DatabaseName });
+
+            PartitionKeyDefinition partitionKey = new PartitionKeyDefinition
+            {
+                Paths = new Collection<string> { $"/{_cosmosConfiguration.PartitionKey}" }
+            };
+            DocumentCollection collection = new DocumentCollection { Id = _cosmosConfiguration.CollectionName, PartitionKey = partitionKey };
+
+            var resourceResponse = await documentClient
+                .CreateDocumentCollectionIfNotExistsAsync($"/dbs/{_cosmosConfiguration.DatabaseName}", collection);
+
+            documentClient.ConnectionPolicy.RetryOptions.MaxRetryWaitTimeInSeconds = 30;
+            documentClient.ConnectionPolicy.RetryOptions.MaxRetryAttemptsOnThrottledRequests = 9;
+
+            GraphBulkExecutor executor = new GraphBulkExecutor(documentClient, resourceResponse.Resource);
+
+            await executor.InitializeAsync();
+
+            documentClient.ConnectionPolicy.RetryOptions.MaxRetryWaitTimeInSeconds = 0;
+            documentClient.ConnectionPolicy.RetryOptions.MaxRetryAttemptsOnThrottledRequests = 0;
+
+            await executor.BulkImportAsync(
+                graphElements,
+                enableUpsert: true,
+                cancellationToken: token
+            );
+
+            return;
+        }
+    }
+}
